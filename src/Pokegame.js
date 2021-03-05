8686512d9bd161f872930dc1aa91840ab90d8f85
Jeremiah Brian <brianjeremy123789@gmail.com>
2021-03-05 23:30:08 +0300

    randomly display pokedex 1 based on pokedex 2

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
- 
+import Pokedex from './Pokedex';
 class Pokegame extends Component{
+    static defaultProps = {
+        pokemon : [
+            {id: 4,name: 'Charmander',type: 'Fire', base_experience:62},
+            {id: 7,name: 'Squirtle',type: 'Water', base_experience:63},
+            {id: 11,name: 'MetaPod',type: 'Bug', base_experience:72},
+            {id: 12,name: 'Butterfree',type: 'flying', base_experience:178},
+            {id: 25,name: 'Pikachu',type: 'electric', base_experience:112},
+            {id: 39,name: 'JIgglyPuff',type: 'normal', base_experience:95},
+            {id: 94,name: 'Gengar',type: 'poison', base_experience:225},
+            {id: 133,name: 'Eevee',type: 'normal', base_experience:65}
+        ]
+    };
+
     render(){
+        let hand1 =[];
+        let hand2 = [...this.props.pokemon];
+
+        while (hand2.length > hand1.length ){
+            let randIdx = Math.floor(Math.random() * hand2.length);
+            let randPokemon = hand2.splice(randIdx, 1)[0];
+            hand1.push(randPokemon);
+
+        }
+        let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience)
         return(
             <div>
-                <h1>Pokegame!</h1>
+               <Pokedex pokemon={hand1}/>
+               <Pokedex pokemon={hand2}/>
             </div>
         ) }
 }
+
 export default Pokegame;
